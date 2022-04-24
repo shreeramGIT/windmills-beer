3458e8f4d95fcb6af8e29a0bde71f5a0b9edd240
shreeram-rout <shreeram.rout@maersk.com>
2022-04-24 21:36:58 +0530

    Initial commit

---
+++ src/main/java/shree/windmillsbeer/WindmillsBeerApplication.java
@@ -0,0 +1,13 @@
+package shree.windmillsbeer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindmillsBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindmillsBeerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shree/windmillsbeer/WindmillsBeerApplicationTests.java
@@ -0,0 +1,13 @@
+package shree.windmillsbeer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindmillsBeerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
